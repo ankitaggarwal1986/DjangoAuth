fb6b902ea83f0cb8674d288b2a47058404190dfe
ankit <ankit.aggarwal1986@hotmail.com>
2022-01-16 19:54:43 +0530

    initial checkin

--- user/user/settings.py
+++ user/user/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#8w(rs%9+z=6&t51tjt$1er)_oioeo*kj(qbz6@)mli$=hh4&'
+SECRET_KEY = 'django-insecure-kp_^qven_73iunpk=vy@(=we+)+(cclg16s53!arl2s5=e@a2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
